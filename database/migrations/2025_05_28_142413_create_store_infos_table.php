0858eedf82885fc695887d3db175b23574816967
Abdalrhman abdalnabe <161177575+AbdalrhmanAbdoAlhade@users.noreply.github.com>
2026-02-12 11:23:36 +0200

    Update 2025_05_28_142413_create_store_infos_table.php

--- database/migrations/2025_05_28_142413_create_store_infos_table.php
+++ database/migrations/2025_05_28_142413_create_store_infos_table.php
@@ -21,6 +21,7 @@ return new class extends Migration
             $table->string('phone');
             $table->string('email');
             $table->string('address');
+            $table->string('color');
             $table->string('facebook')->nullable();
             $table->string('twitter')->nullable();
             $table->string('instagram')->nullable();
